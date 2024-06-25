507005052901b4f8f14806620b9f743e13399616
Xurshid97 <129675148+Xurshid97@users.noreply.github.com>
2024-06-25 15:29:43 +0200

    cred

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -20,7 +20,7 @@ function Home() {
           method: 'GET',
           headers: {
             'Content-Type': 'application/json',
-            'cookie': 'NEXT_LOCALE=en; __Host-next-auth.csrf-token=317b9594a77045155affde5614712f05f0b184f4629b2c5965c863f38a07c67a%7Cd67fc7823950ff436f39fbf03d5dd47f0759e103c016e37f76d0d35b2dab4725; __Secure-next-auth.callback-url=https%3A%2F%2Fconvify-main.server4.xyz%2Fdashboard; __Secure-next-auth.session-token=eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2R0NNIn0..gT_UaHCZjBL-r0Ty.etlk6UmWA9khEbhDVqG2IX-wc9YCKbl4X3EL3uhqldKBdaUFGdnqvS5JNhlm59cc7lhCI4pAEmKNSYPxp6k3QRFLq1dBLq9AfAo9cMYQfO33rdHRxtwOXdwVFQRn2x9bIu3YZf5KttqVLQ6rWMyqIxqPwhQQNLgUHxU6ktRze1tbVDDe5qtYFYWNPJn-HwCjkzeY8h3i-n7rnqK944yk8A.D_ygp5kiyPOyqm87NUHFvQ',
+            credentials: 'include',
           },
         });
 
